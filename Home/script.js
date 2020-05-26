d0a04cfe82ea14b52e1ba6500ce29455c4b03d96
bohorquez866 <57283888+bohorquez866@users.noreply.github.com>
2020-05-25 20:44:18 -0500

    desktop minor changes

--- Home/script.js
+++
@@ -1,9 +0,0 @@
-const submitBtn = document.querySelector('#search-submit');
-const form = document.querySelector('.hero__form');
-
-let submit = form.addEventListener('submit', e => {
-    console.log('funciona');
-});
-
-
-submitBtn.addEventListener('click', submit);
