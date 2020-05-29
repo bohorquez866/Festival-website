ab68259df5fdddb0bcb6b33160e744c51dcb211f
Jesus Bohorquez <bohorquez866@gmail.com>
2020-05-29 17:38:34 -0500

    Home Site complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const titles = document.querySelectorAll('.footer-responsive__title'); //title to click on
+const titles = document.querySelectorAll('.show-content'); //element to click on
 const blockContent = document.querySelectorAll('.showable-content'); // block to show & hide
 arrowToggle = document.querySelectorAll('.toggle-down'); //arrow to be rotated
 
@@ -7,11 +7,11 @@ arrowToggle = document.querySelectorAll('.toggle-down'); //arrow to be rotated
 for (i = 0; i < titles.length; i++) {
     titles[i].addEventListener("click", function(e) {
         const block = this.nextElementSibling;
-        console.log(e.target.vale);
-        if (block.style.display === 'none') {
-            block.style.display = 'block';
-        } else {
+
+        if (block.style.display === 'block') {
             block.style.display = 'none';
+        } else {
+            block.style.display = 'block';
         }
     });
 }
